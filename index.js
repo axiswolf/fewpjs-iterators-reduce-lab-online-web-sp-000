465a9365a7393f2e438e209443a806b072690631
Karen V <karenvo@usc.edu>
2020-01-08 10:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 function reducer(batteryBatches) {
   let totalBatteries = 0;
-  
+
   batteryBatches.forEach(battery => {
     totalBatteries += battery
   });
