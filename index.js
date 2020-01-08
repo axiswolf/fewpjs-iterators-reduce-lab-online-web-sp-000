434bf3f2c06d286c0e0cd401e56250142e26643b
Karen V <karenvo@usc.edu>
2020-01-08 10:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function reducer(batteryBatches) {
-  let totalBatteries = 0;
-
-  batteryBatches.forEach(battery => {
-    totalBatteries += battery;
-  });
-  return totalBatteries;
+// function reducer(batteryBatches) {
+//   let totalBatteries = 0;
+//
+//   batteryBatches.forEach(battery => {
+//     totalBatteries += battery;
+//   });
+//   return totalBatteries;
+// }
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+function batteries(batteryBatches) {
+  return batteryBatches.reduce(reducer);
 }
