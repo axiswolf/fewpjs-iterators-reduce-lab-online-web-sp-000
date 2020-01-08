db62b1afca647d3d4ffb677748a920d565373b97
Karen V <karenvo@usc.edu>
2020-01-08 10:51:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function batteries(batteryBatches) {
+function reducer(batteryBatches) {
   let totalBatteries = 0;
 
   batteryBatches.forEach(battery => {
